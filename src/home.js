0fc4ecf4d3f02016e3b4cbdb1c06897f5b140405
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-26 14:43:51 +0630

    Initial commit -m

---
+++ src/home.js
@@ -0,0 +1,38 @@
+function createLogo() {
+  const h1Element = document.createElement('h1');
+  h1Element.classList.add('logo');
+  h1Element.innerText = 'Classico Italiano';
+
+  return h1Element;
+}
+
+function createHeaderLinks() {
+  const linksContainer = document.createElement('ul');
+  linksContainer.classList.add('links');
+
+  const homeLink = document.createElement('li');
+  homeLink.innerText = 'Home';
+
+  const menuLink = document.createElement('li');
+  menuLink.innerText = 'Menu';
+
+  const contactLink = document.createElement('li');
+  contactLink.innerText = 'Contact';
+
+  linksContainer.append(homeLink, menuLink, contactLink);
+
+  return linksContainer;
+}
+
+function createHeader() {
+  const body = document.body;
+
+  const header = document.createElement('div');
+  header.classList.add('header');
+
+  header.append(createLogo(), createHeaderLinks());
+
+  body.insertBefore(header, body.firstChild);
+}
+
+export {createHeader};

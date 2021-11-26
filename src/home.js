fa45a4d74376346ab9edaff177c67634ad0dc47a
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-26 16:53:48 +0630

    Make nav and footer separate modules

--- src/home.js
+++ src/home.js
@@ -1,39 +1,5 @@
 import HomeImage from '../assets/img/home_page.jpg';
-
-function createLogo() {
-  const h1Element = document.createElement('h1');
-  h1Element.classList.add('logo');
-  h1Element.innerText = 'Classico Italiano';
-
-  return h1Element;
-}
-
-function createHeaderLinks() {
-  const linksContainer = document.createElement('ul');
-  linksContainer.classList.add('links');
-
-  const homeLink = document.createElement('li');
-  homeLink.innerText = 'Home';
-
-  const menuLink = document.createElement('li');
-  menuLink.innerText = 'Menu';
-
-  const contactLink = document.createElement('li');
-  contactLink.innerText = 'Contact';
-
-  linksContainer.append(homeLink, menuLink, contactLink);
-
-  return linksContainer;
-}
-
-function createHeader() {
-  const header = document.createElement('div');
-  header.classList.add('header');
-
-  header.append(createLogo(), createHeaderLinks());
-
-  return header;
-}
+import { createHeader, createFooter } from './navbar_footer';
 
 function createIntro() {
   const content = document.getElementById('content');
@@ -44,13 +10,6 @@ function createIntro() {
   content.appendChild(introImage);
 
   return content;
-}
-
-function createFooter() {
-  const footer = document.createElement('footer');
-  footer.innerText = 'Created By Thant Ko Zaw';
-
-  return footer;
 }
 
 function createHomepage() {
---
+++ src/navbar_footer.js
@@ -0,0 +1,43 @@
+function createLogo() {
+  const h1Element = document.createElement('h1');
+  h1Element.classList.add('logo');
+  h1Element.innerText = 'Classico Italiano';
+
+  return h1Element;
+}
+
+function createHeaderLinks() {
+  const linksContainer = document.createElement('ul');
+  linksContainer.classList.add('links');
+
+  const homeLink = document.createElement('li');
+  homeLink.innerText = 'Home';
+
+  const menuLink = document.createElement('li');
+  menuLink.innerText = 'Menu';
+
+  const contactLink = document.createElement('li');
+  contactLink.innerText = 'Contact';
+
+  linksContainer.append(homeLink, menuLink, contactLink);
+
+  return linksContainer;
+}
+
+function createHeader() {
+  const header = document.createElement('div');
+  header.classList.add('header');
+
+  header.append(createLogo(), createHeaderLinks());
+
+  return header;
+}
+
+function createFooter() {
+  const footer = document.createElement('footer');
+  footer.innerText = 'Created By Thant Ko Zaw';
+
+  return footer;
+}
+
+export {createHeader, createFooter};

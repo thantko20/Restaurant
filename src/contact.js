bfec4d1f3dc18ed41ef0a3ede77c6806469d60c3
Thant Ko Zaw <thantmkou10@gmail.com>
2021-11-27 14:08:47 +0630

    Add contact page

---
+++ src/contact.js
@@ -0,0 +1,17 @@
+export function createContact() {
+  const contactHeader = document.createElement('h2');
+  contactHeader.innerText = 'Contact';
+
+  const infoContainer = document.createElement('div');
+
+  const email = document.createElement('div');
+  email.innerHTML = `<em>Email: </em> example@mail.com`;
+
+  const phoneNumber = document.createElement('div');
+  phoneNumber.innerHTML = `<em>Ph: </em> 012345678`;
+
+  infoContainer.append(email, phoneNumber);
+
+  const content = document.getElementById('content');
+  content.append(contactHeader, infoContainer);
+}

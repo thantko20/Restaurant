d3595dea0d59165970d7e03561d2b22cffc91740
Thant Ko Zaw <thantmkou10@gmail.com>
2022-04-27 19:46:56 +0630

    Add tab switching function

--- src/components/nav/navItem.js
+++ src/components/nav/navItem.js
@@ -1,9 +1,13 @@
+import renderDOM from "../../renderDOM";
+
 const navItem = function (name, classList=[]) {
   const classListStr = classList.join(' ');
 
   const listEl = document.createElement('li');
   listEl.textContent = name;
   listEl.className = `link ${classListStr}`;
+
+  listEl.addEventListener('click', () => renderDOM.renderTab(classList[0]));
 
   return listEl;
 }
--- src/renderDOM.js
+++ src/renderDOM.js
@@ -1,21 +1,38 @@
-import { render } from '@testing-library/react';
-import { header } from './components/header'
+import { header } from './components/header';
+import home from './components/home';
 
 const renderDOM = (function () {
   const body = document.body;
+  const content = document.getElementById('content');
 
   const renderHeader = function () {
     const headerEl = header();
-    console.log(headerEl)
     body.insertBefore(headerEl, body.firstChild);
+  };
+
+  const renderHome = function () {
+    home();
+  };
+
+  const renderTab = function (className) {
+    const tabEl = document.querySelector(`.${className}`);
+    const activeEl = document.querySelector('.active');
+
+    if (tabEl === activeEl) return;
+
+    tabEl.classList.add('active');
+    activeEl.classList.remove('active');
   };
 
   const init = function () {
     renderHeader();
+    renderHome();
   };
 
   return {
     renderHeader,
+    renderHome,
+    renderTab,
     init,
   };
 })();

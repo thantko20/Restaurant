9973832a29b71fb77c9b39e2f292d8b3af329183
Thant Ko Zaw <thantmkou10@gmail.com>
2022-04-27 18:00:06 +0630

    Add header

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import { navBar } from "./nav/navBar";
+
+const logo = function (name) {
+  const el = document.createElement('h1');
+  el.className = 'logo'; 
+  el.textContent = 'Classico Italiano';
+
+  return el;
+}
+
+const header = function () {
+  const container = document.createElement('header');
+  container.className = 'header';
+
+  container.append(logo, navBar);
+
+  return container;
+}
